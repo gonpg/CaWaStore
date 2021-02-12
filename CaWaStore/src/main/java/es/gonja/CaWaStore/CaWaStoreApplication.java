521a569247277d05003da6414e235b4a1d4c1db2
gonzaloperez <mg.perez.2017@alumnos.urjc.es>
2021-02-12 16:45:59 +0100

    Subida de proyecto

---
+++ CaWaStore/src/main/java/es/gonja/CaWaStore/CaWaStoreApplication.java
@@ -0,0 +1,13 @@
+package es.gonja.CaWaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaWaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaWaStoreApplication.class, args);
+	}
+
+}
---
+++ CaWaStore/src/test/java/es/gonja/CaWaStore/CaWaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package es.gonja.CaWaStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaWaStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
