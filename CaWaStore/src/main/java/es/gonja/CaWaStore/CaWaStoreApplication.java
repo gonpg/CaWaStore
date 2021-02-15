e38cb9989ae952ef478fdb9fc91a5c6396f24696
gonzaloperez <mg.perez.2017@alumnos.urjc.es>
2021-02-15 20:52:02 +0100

    Prueba commit 2

--- CaWaStore/src/main/java/es/gonja/CaWaStore/CaWaStoreApplication.java
+++ CaWaStore/src/main/java/es/gonja/CaWaStore/CaWaStoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CaWaStoreApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hola");
 		SpringApplication.run(CaWaStoreApplication.class, args);
 	}
 
