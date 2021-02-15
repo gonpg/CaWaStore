426a2077353c1513777597384adb473da583da63
joseangel <ja.sanchezp.2017@alumnos.urjc.es>
2021-02-15 21:00:55 +0100

    Prueba commit 3

--- CaWaStore/src/main/java/es/gonja/CaWaStore/CaWaStoreApplication.java
+++ CaWaStore/src/main/java/es/gonja/CaWaStore/CaWaStoreApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CaWaStoreApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hola");
+		System.out.println("Hola Mundoooooo");
 		SpringApplication.run(CaWaStoreApplication.class, args);
 	}
 
