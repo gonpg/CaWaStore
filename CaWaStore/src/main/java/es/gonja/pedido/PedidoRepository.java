9057349d4256eb4521b4359ba25d0af94f5de02a
joseangelsanchezperez <78819378+joseangelsanchezperez@users.noreply.github.com>
2021-03-08 22:02:54 +0100

    Delete CaWaStore directory

--- CaWaStore/src/main/java/es/gonja/pedido/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package es.gonja.pedido;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-}
--- CaWaStore/src/main/java/es/gonja/resena/ResenaRepository.java
+++
@@ -1,8 +0,0 @@
-package es.gonja.resena;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-
-public interface ResenaRepository extends JpaRepository<Resena, Long> {
-}
