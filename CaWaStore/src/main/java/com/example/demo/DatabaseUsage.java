0762d736a103895ba9f9b3deaebdf38513edef78
gonpg <mg.perez.2017@alumnos.urjc.es>
2021-03-08 22:34:37 +0100

    Update DatabaseUsage.java

--- CaWaStore/src/main/java/com/example/demo/DatabaseUsage.java
+++ CaWaStore/src/main/java/com/example/demo/DatabaseUsage.java
@@ -22,9 +22,7 @@ import java.util.List;
 @Controller
 public class DatabaseUsage implements CommandLineRunner {
 
-	@Autowired
-	private PedidoRepository pedidoRepository;
-	 
+	
 	@Autowired
     private UsuarioRepository usuarioRepository;
 
@@ -33,7 +31,10 @@ public class DatabaseUsage implements CommandLineRunner {
 
     @Autowired
     private ResenaRepository resenaRepository;
-
+    
+    @Autowired
+	private PedidoRepository pedidoRepository;
+	 
     @Autowired
     private PromocionRepository promocionRepository;
 
@@ -42,66 +43,45 @@ public class DatabaseUsage implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        Usuario u1 = new Usuario("bustamante_", "liz@sen.ma.gov", "12345678");
-        Usuario u2 = new Usuario("pepe28", "pepe@gmail.com", "12345678");
-        Usuario u3 = new Usuario("juan98", "juan98@gmail.com", "12345678");
-        Usuario u4 = new Usuario("santi_ago", "santi00@outlook.es", "12345678");
-        Usuario u5 = new Usuario("alonso12", "alonso12@gmail.es", "12345678");
+        Usuario u1 = new Usuario("joseAngel", "jose@hola.com", "12345678");
+        Usuario u2 = new Usuario("pepe28", "gonzalo@hola.com", "12345678");
+       
 
         usuarioRepository.save(u1);
         usuarioRepository.save(u2);
-        usuarioRepository.save(u3);
-        usuarioRepository.save(u4);
-        usuarioRepository.save(u5);
+       
 
 
-        Producto p1 = new Producto("Solán de Cabras", "alimentación", 1.2F, 5,
-                "Agua mineral natural.",
-                "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201901/21/00110237800353____1__640x640.jpg");
+        Producto p1 = new Producto("Barras paralelas", "Crossfit", 40.5F, 5,
+                "Barras paralelas",
+                "https://contents.mediadecathlon.com/p1103588/k$5cc7c279f32cbbd44a5abc4ab065cd54/sq/Barras+paralelas+para+dips+cross+training+musculaci+n+training+station+100.jpg");
 
-        Producto p2 = new Producto("Chocolate Milka", "alimentación", 0.99F, 3,
-                "Puede contener Gluten y trazas de frutos secos.",
-                "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201808/10/00120646700896____22__600x600.jpg");
+        Producto p2 = new Producto("Balón medicinal 3KG", "Mancuernas", 10.65F, 3,
+                "Balon medicinal 3KG",
+                "https://contents.mediadecathlon.com/p1175605/k$26172214da5a9910dd5ab780e42ed8a0/sq/BAL+N+MEDICINAL+3+kg+Di+metro+22+cm+AMARILLO.jpg");
 
-        Producto p3 = new Producto("BUSTAMANTE Muy Mío", "higiene", 6.95F, 111,
-                "Un seductor natural que despierta pasiones. Confianza en sí mismo, entrega, emperamento, carisma, naturalidad, espíritu de lucha? concentrados en una fragancia.",
-                "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201909/23/00155760200737____1__600x600.jpg");
+        Producto p3 = new Producto("Chaleco de 5KG", "Mancuernas", 30.5F, 100,
+                "Chaleco lastrado con pesas de 5KG",
+                "https://contents.mediadecathlon.com/p1643571/k$3c00962c129eb3e87475fd6769961ebd/sq/Chaleco+lastrado+cross+training+musculaci+n+5+kg.jpg");
 
-        Producto p4 = new Producto("DODOT Activity toallitas infantiles", "higiene", 8.63F, 56,
-                "Máxima comodidad y protección en la piel, su capa absorvente cora-soft es suave como una pluma.",
-                "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201905/10/00155763100421____15__600x600.jpg");
-
-        Producto p5 = new Producto("ASTURIANA leche semidesnatada", "alimentación", 0.89F, 23,
-                "Leche UHT semidesnatada sin lactosa",
-                "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201902/07/00120912000260____23__600x600.jpg");
-
-        Producto p6 = new Producto("Vino blanco", "alimentación", 13.90F, 45,
-                "Color amarillo verdoso pálido con destellos alimonados, brillante, limpio y cristalino. Aromas de intensidad media con claras notas minerales.",
-                "https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201909/24/00118775700711____2__600x600.jpg");
-
+        
         productoRepository.save(p1);
         productoRepository.save(p2);
         productoRepository.save(p3);
-        productoRepository.save(p4);
-        productoRepository.save(p5);
-        productoRepository.save(p6);
+       
 
 
         LocalDate localDate = java.time.LocalDate.now();
         Date date = java.sql.Date.valueOf(localDate);
 
-        Resena r1 = new Resena(p1, u1, date, "Cumple su funcion");
-        Resena r2 = new Resena(p2, u2, date, "Cumple su funcion");
-        Resena r3 = new Resena(p3, u3, date, "Cumple su funcion");
-        Resena r4 = new Resena(p4, u4, date, "Cumple su funcion");
-        Resena r5 = new Resena(p5, u5, date, "Cumple su funcion");
-
+        Resena r1 = new Resena(p1, u1, date, "Buen producto");
+        Resena r2 = new Resena(p2, u2, date, "Buen producto");
+       
+       
         resenaRepository.save(r1);
         resenaRepository.save(r2);
-        resenaRepository.save(r3);
-        resenaRepository.save(r4);
-        resenaRepository.save(r5);
-
+        
+        
 
         List<Producto> l1 = new ArrayList<>();
         l1.add(p1);
@@ -109,16 +89,15 @@ public class DatabaseUsage implements CommandLineRunner {
 
         List<Producto> l2 = new ArrayList<>();
         l2.add(p3);
-        l2.add(p4);
-        l2.add(p5);
+      
 
         List<Producto> l3 = new ArrayList<>();
         l3.add(p3);
 
         Pedido o1 = new Pedido(u1, date, l1);
-        Pedido o2 = new Pedido(u3, date, l2);
-        Pedido o3 = new Pedido(u4, date, l3);
-        Pedido o4 = new Pedido(u3, date, l3);
+        Pedido o2 = new Pedido(u2, date, l2);
+        Pedido o3 = new Pedido(u1, date, l3);
+        Pedido o4 = new Pedido(u2, date, l3);
 
         pedidoRepository.save(o1);
         pedidoRepository.save(o2);
@@ -126,25 +105,12 @@ public class DatabaseUsage implements CommandLineRunner {
         pedidoRepository.save(o4);
 
 
-        Promocion pr1 = new Promocion(20);
+        Promocion pr1 = new Promocion(30);
         pr1.setProducto(p3);
 
         promocionRepository.save(pr1);
-
-
-        List<Producto> query = productoRepository.findAll();
-        for (Producto p : query) {
-            System.out.println("PRODUCTO= " + p.getNombre());
-        }
-
-        List<Resena> query2 = resenaRepository.findAll();
-        for (Resena r : query2) {
-            System.out.println("REVIEW= " + "producto: " + r.getProducto().getNombre() + " | " + r.getTexto());
-        }
-
-        List<Promocion> query3 = promocionRepository.findAll();
-        for (Promocion r : query3) {
-            System.out.println("PROMOTION= " + "producto: " + r.getProducto().getNombre() + " | " + r.getPromocion());
-        }
     }
 }
+
+
+       
