7432523e4a5017cd45f4af9735875c8920ea6eb6
gonpg <mg.perez.2017@alumnos.urjc.es>
2021-04-09 19:17:19 +0200

    Proteccion CSRF habilitado

--- CaWaStore/src/main/java/com/example/SecurityConfiguration.java
+++ CaWaStore/src/main/java/com/example/SecurityConfiguration.java
@@ -6,12 +6,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class Seguridad extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // Páginas públicacs:
-    	
+        // Páginas públicas:
         http.authorizeRequests().antMatchers("/").permitAll();
         http.authorizeRequests().antMatchers("/productos").permitAll();
         http.authorizeRequests().antMatchers("/productos/{id}").permitAll();
@@ -20,18 +19,13 @@ public class Seguridad extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/loginerror").permitAll();
         http.authorizeRequests().antMatchers("/logout").permitAll();
         http.authorizeRequests().antMatchers("/signup.html").permitAll();
-
         // Páginas privadas(el resto)
         http.authorizeRequests().anyRequest().authenticated();
-
         // Login
         http.formLogin().defaultSuccessUrl("/productos");
-
         // Logout
         http.logout().logoutUrl("/logout");
         http.logout().logoutSuccessUrl("/productos");
-
-        // Inhabilitar CSRF
-        http.csrf().disable();
+       
     }
 } 
